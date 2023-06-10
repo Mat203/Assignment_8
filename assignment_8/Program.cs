1c0caf34f5a8b018370a7cb543ae2625d6d9157f
smazepaa <smazepa@kse.ord.ua>
2023-06-10 21:45:09 +0300

    basic program(with spell checker) + changed a bit in userPreferences

--- assignment_8/Program.cs
+++ assignment_8/Program.cs
@@ -63,22 +63,32 @@ Dictionary<string, double[]> GetUserPreferences(List<UserRating> userRatings, Li
         "Romance", "Animation", "Thriller"};
 
     var userPreferences = new Dictionary<string, double[]>();
+    var userSum = new Dictionary<string, double>();
     
     foreach(var userRating in userRatings)
     {
         if (!userPreferences.ContainsKey(userRating.user_id))
+        {
             userPreferences[userRating.user_id] = new double[8];
+            userSum[userRating.user_id] = 0;
+        }
         
         var movieGenre = listMovies.Find(m => m.movie_id == userRating.movie_id)?.genres;
-        
+
         if (movieGenre != null && gen.Contains(movieGenre))
+        {
             userPreferences[userRating.user_id][gen.IndexOf(movieGenre)] += userRating.rating_val;
+            userSum[userRating.user_id] += userRating.rating_val;
+        }
     }
     
     foreach(var userId in userPreferences.Keys.ToList())
     {
-        for(int i = 0; i < 8; i++)
-            userPreferences[userId][i] /= 10.0; //divide by 10 cuz we need point from 0 to 1
+        for (int i = 0; i < 8; i++)
+        {
+            userPreferences[userId][i] /= userSum[userId];
+            userPreferences[userId][i] = Math.Round(userPreferences[userId][i], 5);
+        }
     }
     
     return userPreferences;
@@ -93,6 +103,90 @@ foreach(var userId in userPreferences.Keys)
 {
     Console.WriteLine(userId + ": [" + string.Join(", ", userPreferences[userId]) + "]");
 }
+
+
+// basic program
+Console.Write("> ");
+var input = Console.ReadLine();
+var splitted = input.Split(' ');
+var command = splitted[0];
+var rate = Convert.ToDouble(splitted[^1]);
+var movie = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(string.Join(" ", splitted[1..^1]));
+// we need this long thing so that every word starts with the capital letter and later it was easier to compare 
+var movieId = "";
+
+
+foreach (var el in listMovies)
+{
+    if (el.movie_title == movie)
+    {
+        movieId = el.movie_id;
+        break;
+    }
+}
+
+if (command == "rate")
+{
+    if (movieId == "")
+    {
+        Console.WriteLine("no such movie found :( closest candidates:");
+        spellChecker(movie);
+    }
+    else
+    {
+        Console.WriteLine($"> you've rated a film '{movie}' ({movieId}) as {rate}");
+    }
+    
+}
+
+void spellChecker(string movieTitle)
+{
+    var distances = new Dictionary<string, int>();
+    var listTitles = listMovies.Select(movie1 => movie1.movie_title).ToList();
+        
+    foreach (var title in listTitles)
+    {
+        distances[title] = DamerauLevenshteinDistance(movieTitle, title);
+    }
+    
+    var sortedKeyValuePairs = distances.OrderBy(x => x.Value).Take(3).ToList();
+    var suggestions = sortedKeyValuePairs.Select(pair => pair.Key).ToList();
+
+    Console.WriteLine(string.Join(", ", suggestions));
+}
+
+
+int DamerauLevenshteinDistance(string word1, string word2)
+{
+    var w1 = word1.Length;
+    var w2 = word2.Length;
+
+    var matrix = new int[w1 + 1, w2 + 1]; 
+    
+    for (var j = 0; j <= w2; j++)
+        matrix[0, j] = j; // прописує рядок з індексами
+    for (var i = 0; i <= w1; i++)
+        matrix[i, 0] = i; // прописує стовбець з індексами
+
+    for (var j = 1; j <= w2; j++)
+    {
+        for (var i = 1; i <= w1; i++)
+        {
+            var cost = word1[i - 1] == word2[j - 1] ? 0 : 1;
+            
+            matrix[i, j] = Math.Min(
+                Math.Min(matrix[i - 1, j] + 1, matrix[i, j - 1] + 1),
+                matrix[i - 1, j - 1] + cost);
+            
+            if (i > 1 && j > 1 && word1[i - 1] == word2[j - 2] && word1[i - 2] == word2[j - 1])
+            {
+                matrix[i, j] = Math.Min(matrix[i, j], matrix[i - 2, j - 2] + cost);
+            }
+        }
+    }
+    return matrix[w1 , w2];
+}
+
 
 public class Movie
 {
