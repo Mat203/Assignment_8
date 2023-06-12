9a8ced46a0be54e34e8dfefc1b1a5047046319fb
Mat_Tol <matvey.talalaevskiy@gmail.com>
2023-06-12 22:24:30 +0300

    User preparation & calculation in space

--- assignment_8/Program.cs
+++ assignment_8/Program.cs
@@ -64,15 +64,15 @@ Dictionary<string, double[]> GetUserPreferences(List<UserRating> userRatings, Li
 
     var userPreferences = new Dictionary<string, double[]>();
     var userSum = new Dictionary<string, double>();
-    
-    foreach(var userRating in userRatings)
+
+    foreach (var userRating in userRatings)
     {
         if (!userPreferences.ContainsKey(userRating.user_id))
         {
             userPreferences[userRating.user_id] = new double[8];
             userSum[userRating.user_id] = 0;
         }
-        
+
         var movieGenre = listMovies.Find(m => m.movie_id == userRating.movie_id)?.genres;
 
         if (movieGenre != null && gen.Contains(movieGenre))
@@ -81,8 +81,8 @@ Dictionary<string, double[]> GetUserPreferences(List<UserRating> userRatings, Li
             userSum[userRating.user_id] += userRating.rating_val;
         }
     }
-    
-    foreach(var userId in userPreferences.Keys.ToList())
+
+    foreach (var userId in userPreferences.Keys.ToList())
     {
         for (int i = 0; i < 8; i++)
         {
@@ -90,7 +90,7 @@ Dictionary<string, double[]> GetUserPreferences(List<UserRating> userRatings, Li
             userPreferences[userId][i] = Math.Round(userPreferences[userId][i], 5);
         }
     }
-    
+
     return userPreferences;
 }
 
@@ -99,56 +99,86 @@ var listMovies = ArrangeMovies(m);
 var userRatings = GetUserRatings("rating.csv");
 var userPreferences = GetUserPreferences(userRatings, listMovies);
 
-foreach(var userId in userPreferences.Keys)
+foreach (var userId in userPreferences.Keys)
 {
     Console.WriteLine(userId + ": [" + string.Join(", ", userPreferences[userId]) + "]");
 }
 
 
-// basic program
-Console.Write("> ");
-var input = Console.ReadLine();
-var splitted = input.Split(' ');
-var command = splitted[0];
-var rate = Convert.ToDouble(splitted[^1]);
-var movie = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(string.Join(" ", splitted[1..^1]));
-// we need this long thing so that every word starts with the capital letter and later it was easier to compare 
-var movieId = "";
+Recommend();
 
 
-foreach (var el in listMovies)
+void Recommend()
 {
-    if (el.movie_title == movie)
+    List<UserRating> new_user = new List<UserRating>();
+    int count = 0;
+    while (true)
     {
-        movieId = el.movie_id;
-        break;
-    }
-}
+        Console.Write("> ");
+        var input = Console.ReadLine();
+        if (input == "recommend" && count > 2)
+        {
+            var new_userPreferences = GetUserPreferences(new_user, listMovies);
+            foreach (var userId in new_userPreferences.Keys)
+            {
+                Console.WriteLine(userId + ": [" + string.Join(", ", new_userPreferences[userId]) + "]");
+            }
+            break;
+        }
+        else
+        {
+            var splitted = input.Split(' ');
+            var command = splitted[0];
+            var rate = Convert.ToDouble(splitted[^1]);
+            var movie = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(string.Join(" ", splitted[1..^1]));
+            var movieId = "";
+            foreach (var el in listMovies)
+            {
+                if (el.movie_title == movie)
+                {
+                    movieId = el.movie_id;
+                    break;
+                }
+            }
 
-if (command == "rate")
-{
-    if (movieId == "")
-    {
-        Console.WriteLine("no such movie found :( closest candidates:");
-        spellChecker(movie);
+            if (command == "rate")
+            {
+                if (movieId == "")
+                {
+                    Console.WriteLine("no such movie found :( closest candidates:");
+                    spellChecker(movie);
+                }
+                else
+                {
+                    UserRating rating1 = new UserRating
+                    {
+                        _id = "0",
+                        movie_id = movieId,
+                        rating_val = (int)rate,
+                        user_id = "user123"
+                    };
+                    new_user.Add(rating1);
+                    Console.WriteLine($"> you've rated a film '{movie}' ({movieId}) as {rate}");
+
+                    count++;
+                    Console.WriteLine(count);
+                }
+            }
+        }
+
     }
-    else
-    {
-        Console.WriteLine($"> you've rated a film '{movie}' ({movieId}) as {rate}");
-    }
-    
 }
 
 void spellChecker(string movieTitle)
 {
     var distances = new Dictionary<string, int>();
     var listTitles = listMovies.Select(movie1 => movie1.movie_title).ToList();
-        
+
     foreach (var title in listTitles)
     {
         distances[title] = DamerauLevenshteinDistance(movieTitle, title);
     }
-    
+
     var sortedKeyValuePairs = distances.OrderBy(x => x.Value).Take(3).ToList();
     var suggestions = sortedKeyValuePairs.Select(pair => pair.Key).ToList();
 
@@ -161,8 +191,8 @@ int DamerauLevenshteinDistance(string word1, string word2)
     var w1 = word1.Length;
     var w2 = word2.Length;
 
-    var matrix = new int[w1 + 1, w2 + 1]; 
-    
+    var matrix = new int[w1 + 1, w2 + 1];
+
     for (var j = 0; j <= w2; j++)
         matrix[0, j] = j; // прописує рядок з індексами
     for (var i = 0; i <= w1; i++)
@@ -173,18 +203,18 @@ int DamerauLevenshteinDistance(string word1, string word2)
         for (var i = 1; i <= w1; i++)
         {
             var cost = word1[i - 1] == word2[j - 1] ? 0 : 1;
-            
+
             matrix[i, j] = Math.Min(
                 Math.Min(matrix[i - 1, j] + 1, matrix[i, j - 1] + 1),
                 matrix[i - 1, j - 1] + cost);
-            
+
             if (i > 1 && j > 1 && word1[i - 1] == word2[j - 2] && word1[i - 2] == word2[j - 1])
             {
                 matrix[i, j] = Math.Min(matrix[i, j], matrix[i - 2, j - 2] + cost);
             }
         }
     }
-    return matrix[w1 , w2];
+    return matrix[w1, w2];
 }
 
 
